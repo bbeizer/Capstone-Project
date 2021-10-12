3d8c343d8a3a68f53b558aa271279f7fda85295e
bbeizer <89622436+bbeizer@users.noreply.github.com>
2021-10-11 22:29:29 -0400

    Delete config directory

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :student_alumni_bridges
-  resources :alumnis
-  resources :students
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
